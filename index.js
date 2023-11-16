129fcf4047a7413de81d47c0e9e3c6356ffe1a32
amirn003 <amir.naar@gmail.com>
2023-11-16 12:32:49 +0100

    catch speech detection word

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', (e) => {
+  //console.log(e);
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+
+
+  console.log(transcript);
+})
+
+recognition.start();
